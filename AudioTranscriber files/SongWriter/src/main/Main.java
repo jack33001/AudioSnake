3258abb7285c6cd2687489d2ef80c74f7e204271
MensionWhat <joshuagpawlak@gmail.com>
2019-06-10 09:21:38 -0700

    fix this

--- AudioTranscriber files/SongWriter/src/main/Main.java
+++ AudioTranscriber files/SongWriter/src/main/Main.java
@@ -1,13 +1,28 @@
 package main;
 
 import util.AePlayWave;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.sound.sampled.AudioSystem;
+
 import constants.Constants;
+import jackstuff.Fourier;
 import notes.BPMDetector;
+import notes.Song;
 import snake.Snake;
 import userio.MusicPlayer;
 
 public class Main {
-	public static void main(String args[]) {
-		MusicPlayer mp = new MusicPlayer();
+	public static void main(String args[]) throws FileNotFoundException, Exception {
+		
+		Song music = new Song(Fourier.FFTFile(Constants.WAVFILE_LOCATION));
+		BPMDetector detector = new BPMDetector(Constants.WAVFILE_LOCATION); //create BPMDetector object (automatically finds all beats)
+		Snake snake = new Snake(detector, music); //create snake object
+		new AePlayWave(Constants.WAVFILE_LOCATION).start();
+		
+		snake.startSnake(); //start the snake rendering
+		//MusicPlayer mp = new MusicPlayer();
 	}
 }
